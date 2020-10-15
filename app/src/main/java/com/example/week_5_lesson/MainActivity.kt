7a35a16b9f004965e4601fa06db7fc5fe7223dff
Izaak <McCarthyizaak@gmail.com>
2020-10-15 13:44:00 +0100

    second commit

---
+++ app/src/main/java/com/example/week_5_lesson/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.week_5_lesson
+
+//3 commit
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
